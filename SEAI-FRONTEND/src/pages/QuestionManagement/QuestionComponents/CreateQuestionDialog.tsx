e70a9c1affa453efe891d6d8d4c38c9d8ddde65a
erdalgumuss <erdalgumus777@gmail.com>
2024-11-20 23:08:57 +0300

    Sorular çekilebiliyor. temel giriş çıkış işlevleri. kod düzeni veya daha çok karışması falan, Nevada 20-11-2024 23.04 ölmek üzereyim

--- SEAI-FRONTEND/src/pages/QuestionManagement/QuestionComponents/CreateQuestionDialog.tsx
+++ SEAI-FRONTEND/src/pages/QuestionManagement/QuestionComponents/CreateQuestionDialog.tsx
@@ -35,7 +35,7 @@ export default function CreateQuestionDialog({ isOpen, onOpenChange, onCreateQue
       aiGenerated: true,
     });
   };
-
+//
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-[525px]">
